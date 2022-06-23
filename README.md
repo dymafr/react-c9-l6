2083df2d1bf9d3b77abbabca50f4560628fe929d
dymafr <erwan1309@hotmail.com>
2022-06-23 16:54:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-yn25nh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-yn25nh)
---
+++ src/components/TodoList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import EditTodo from './EditTodo';
+
+export default function TodoList({
+  todoList,
+  deleteTodo,
+  toggleTodo,
+  toggleTodoEdit,
+  editTodo,
+}) {
+  return todoList.length ? (
+    <ul>
+      {todoList.map((todo) =>
+        todo.edit ? (
+          <EditTodo
+            key={todo.id}
+            todo={todo}
+            editTodo={(content) => editTodo(todo.id, content)}
+            cancelEditTodo={() => toggleTodoEdit(todo.id)}
+          />
+        ) : (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            deleteTodo={() => deleteTodo(todo.id)}
+            toggleTodo={() => toggleTodo(todo.id)}
+            editTodo={() => toggleTodoEdit(todo.id)}
+          />
+        )
+      )}
+    </ul>
+  ) : (
+    <p>Aucune tâche en cours </p>
+  );
+}
